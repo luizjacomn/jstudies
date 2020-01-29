c9f3e06df262aefb7d0fd5bde0add40ee619ae0f
Luiz Jacó <luizjacomn@gmail.com>
2020-01-29 16:37:31 -0300

    Thread para buscar termo em arquivo

--- threads/src/main/java/com/luizjacomn/threads/main/Main.java
+++ threads/src/main/java/com/luizjacomn/threads/main/Main.java
@@ -1,9 +1,18 @@
 package com.luizjacomn.threads.main;
 
-public class Main {
+import com.luizjacomn.threads.task.BuscarEmArquivo;
 
+public class Main {
 	public static void main(String[] args) {
+		String busca = "it";
+
+		Thread buscaJogadores = new Thread(new BuscarEmArquivo("jogadores-futebol.txt", busca));
+		Thread buscaPilotos = new Thread(new BuscarEmArquivo("pilotos.txt", busca));
+		Thread buscaPersonagens = new Thread(new BuscarEmArquivo("personagens-animes.txt", busca));
 		
+		buscaJogadores.start();
+		buscaPilotos.start();
+		buscaPersonagens.start();
 	}
 
 }
---
+++ threads/src/main/java/com/luizjacomn/threads/task/BuscarEmArquivo.java
@@ -0,0 +1,45 @@
+package com.luizjacomn.threads.task;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class BuscarEmArquivo implements Runnable {
+
+	private static final Logger LOGGER = Logger.getLogger(BuscarEmArquivo.class.getSimpleName());
+
+	private String nomeArquivo;
+	private String termo;
+
+	public BuscarEmArquivo(String nomeArquivo, String termo) {
+		this.nomeArquivo = nomeArquivo;
+		this.termo = termo;
+	}
+
+	@Override
+	public void run() {
+		try (Scanner scanner = new Scanner(getClass().getClassLoader().getResourceAsStream(nomeArquivo))) {
+			StringBuilder builder = new StringBuilder(
+					String.format("### Buscar \"%s\" no arquivo \"%s\" ###", termo, nomeArquivo));
+
+			int lineNumber = 1;
+			boolean found = false;
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+
+				if (line.toLowerCase().contains(termo.toLowerCase())) {
+					found = true;
+					builder.append("\n").append(lineNumber).append(" - ").append(line);
+				}
+
+				lineNumber++;
+			}
+
+			LOGGER.info(builder.append("\n").toString());
+
+			if (!found)
+				LOGGER.warning(String.format("Nenhum resultado encontrado no arquivo \"%s\" para o termo \"%s\".",
+						nomeArquivo, termo));
+		}
+	}
+
+}
