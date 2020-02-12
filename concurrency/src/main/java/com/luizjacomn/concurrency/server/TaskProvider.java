6a74d9b7890049928361ce94f51f26e63cc30e02
Luiz Jacó <luizjacomn@gmail.com>
2020-02-12 09:14:48 -0300

    Adicionando comunicação de duas vias com o servidor

--- concurrency/src/main/java/com/luizjacomn/concurrency/server/TaskProvider.java
+++ concurrency/src/main/java/com/luizjacomn/concurrency/server/TaskProvider.java
@@ -1,6 +1,7 @@
 package com.luizjacomn.concurrency.server;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -19,13 +20,31 @@ public class TaskProvider implements Runnable {
 
 			Scanner scanner = new Scanner(socket.getInputStream());
 			
+			PrintStream out = new PrintStream(socket.getOutputStream());
+			
 			while (scanner.hasNextLine()) {
 				String comandoRecebido = scanner.nextLine();
-				System.out.println(comandoRecebido);
+				
+				switch (comandoRecebido) {
+				case "c1":
+					out.println("imprimindo c1");
+					break;
+				case "c2":
+					out.println("imprimindo c2");
+					break;
+				default:
+					out.println("Comando não encontrado!");
+					break;
+				}
+				
+				System.out.println(String.format("Cliente: %s", socket.getRemoteSocketAddress()));
+				System.out.println(String.format("Comando recebido: %s", comandoRecebido));
+				System.out.println("------------------------------------------------");
 			}
 
-			Thread.sleep(20_000);
-		} catch (InterruptedException | IOException e) {
+			scanner.close();
+			out.close();
+		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
